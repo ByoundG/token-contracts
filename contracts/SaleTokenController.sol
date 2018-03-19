3e8133d300e2ed8bdca0654586a048d457e3cbc0
Alice Henshaw <henshawalice@gmail.com>
2018-03-19 16:27:01 -0400

    added another function to the abstract contract

--- contracts/SaleTokenController.sol
+++ contracts/SaleTokenController.sol
@@ -17,15 +17,15 @@ contract SaleTokenController is TokenControllerI {
     /// @dev (overwrites method in TokenControllerI).
     /// @return True if the transfer is allowed
     function transferAllowed(address _from, address _to) external view returns (bool) {
-        if (!hasContributedToPolicy(msg.sender) && sale.hasContributed[msg.sender]) {
-            return false;
-        } else {
-            return true;
-        }
+        return (!sale.hasContributed[_from] || recipientAllowed(_to) || hasUsedProtocol(_from));
     }
 
-    ///@dev Specifies whether a sale contibutor has contributed to the team's policy
-    ///@return True if they have
-    function hasContributedToPolicy(address contributor) internal view returns (bool);
-    
+    ///@dev Specifies whether a sale contibutor has contributed to the team's protocol
+    ///@return True if they have ( they can now transfer their token )
+    function hasUsedProtocol(address contributor) internal view returns (bool);
+
+    ///@dev Specifies whether a recipient of a transfer is allowed
+    ///@dev this allows the possibility of allowing transfers to the team's protocol
+    function recipientAllowed(address recipient) internal view returns (bool);
+
 }
