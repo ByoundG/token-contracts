87fca90a3d0ec7779f26c5d4352de658576a21ab
vdrg <vrdragicevic@gmail.com>
2018-04-05 18:49:54 -0300

    Make Token inherit from DetailledERC20

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.4.19;
 
+import "zeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
 import "./ControllableToken.sol";
 
 
@@ -8,13 +9,18 @@ import "./ControllableToken.sol";
  *
  * @dev ControllableToken is a StandardToken, an OpenZeppelin ERC20 implementation: https://github.com/OpenZeppelin/zeppelin-solidity/tree/master/contracts/token/ERC20
  */
-contract Token is ControllableToken {
+contract Token is ControllableToken, DetailedERC20 {
 
 	/**
 	* @dev Transfer is an event iherited from ERC20Basic.sol interface (OpenZeppelin).
 	* @param _supply Total supply of tokens.
 	*/
-    function Token(uint256 _supply) public {
+    function Token(
+        uint256 _supply,
+        string _name,
+        string _symbol,
+        uint8 _decimals
+    ) DetailedERC20(_name, _symbol, _decimals) public {
         require(_supply != 0);
         totalSupply_ = _supply;
         balances[msg.sender] = _supply;
